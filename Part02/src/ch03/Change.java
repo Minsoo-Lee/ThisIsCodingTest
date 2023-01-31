ee46ddf1c59da71d14b034028691fda81e71fa9c
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-02-01 00:10:46 +0900

    Part02.Ch03 예제3-1 거스름돈

--- Part02/src/ch03/Change.java
+++ Part02/src/ch03/Change.java
@@ -1,16 +1,15 @@
-package Part02.Ch03_Greedy;
+package ch03;
 
 public class Change {
-    public static void main(String[] args) {
-        int n = 1260;
+    public static int solution(int n) {
         int count = 0;
 
-        // 큰 단위의 화폐부터 차례대로 확인
         int[] list = {500, 100, 50, 10};
+
         for (int coin : list) {
             count += n / coin;
             n %= coin;
         }
-        System.out.println(count);
+        return count;
     }
 }
