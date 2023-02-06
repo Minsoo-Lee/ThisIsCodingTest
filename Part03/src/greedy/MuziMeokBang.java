069299c4867e774134afeb4adb81c819190d30a1
Minsoo-Lee <minsoo.james1101@gmail.com>
2023-02-06 21:25:36 +0900

    Part03.Ch11 [무지의 먹방 라이브]

---
+++ Part03/src/greedy/MuziMeokBang.java
@@ -0,0 +1,22 @@
+package greedy;
+
+import java.util.Arrays;
+
+public class MuziMeokBang {
+    public static int solution(int k, int[] foodTime) {
+        int answer = 0;
+        int count = 0;
+        int sec = 0;
+        int len = foodTime.length;
+
+        while (sec < k) {
+            if (Arrays.stream(foodTime).sum() == 0)
+                return -1;
+            if (foodTime[sec % len] == 0)
+                sec++;
+            else
+                foodTime[(sec++) % len]++;
+        }
+        return (++sec) % len;
+    }
+}
